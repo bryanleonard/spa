324a230781583ec4e45892df777859a1ea5192df
Bryan Leonard <b.leonard@zoho.com>
2016-02-08 11:18:24 -0500

    Update comments, test sub-pub in html

--- js/spa.model.js
+++ js/spa.model.js
@@ -1,3 +1,6 @@
+// * chat_model - the chat model object provides methods to interct with our instant messaging.
+// * people_model - the people model object which provides methods to interact with the list of people the model maintains
+
 spa.model = (function() {
 	return {};
 }());
