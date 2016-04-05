3d8aab6b148e4b88b1f772a4535c33452266ba97
Bryan Leonard <bryanleonard.us@gmail.com>
2016-04-05 12:45:44 -0400

    Creating routes for user obj.

--- webapp/app.js
+++ webapp/app.js
@@ -10,6 +10,7 @@ var http    = require('http'),
 	app     = express(),
 	server  = http.createServer(app);
 
+
 // Server config
 app.configure(function() {
 	app.use(express.bodyParser()); 		// decodes forms
@@ -17,6 +18,7 @@ app.configure(function() {
 	app.use(express.static(__dirname + '/public')); //root directory for static files as <current_directory>/public
 	app.use(app.router);						   // Add the router middleware after the static files
 });
+
 
 // For dev environment
 app.configure('development', function() {
@@ -27,15 +29,39 @@ app.configure('development', function() {
 	}));
 });
 
+
 // For prod environment
 app.configure('production', function() {
 	app.use(express.errorHandler);
 });
 
+
+// all configurations below are for routes
 app.get('/', function(req, res) {
 	res.redirect('/spa.html');
 });
 
+app.get('/user/list', function(req, res) {
+	res.contentType('json');
+	res.send({title: 'user list'});
+});
+
+// create a user obj
+app.post('/user/create/', function(req, res) {
+	res.contentType('json');
+	res.send({title: 'user created'})
+});
+
+// read a user obj
+app.get('/user/read/:id', function(req, res) {
+	res.contentType('json');
+	response.send({
+		title: 'user with id' + req.params.id + ' found'
+	});
+});
+
+
+// ---------------
 // Fire it up, man
 server.listen(3000);
 console.log(
