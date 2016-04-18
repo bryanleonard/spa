422fc40613c9f7ed6d5e7a167c42733e2725bebc
Bryan Leonard <bryanleonard.us@gmail.com>
2016-04-18 13:01:29 -0400

    Threw a plugin in there for funsies.

---
+++ js/lib/jquery.tabs-0.0.1.js
@@ -0,0 +1,36 @@
+;(function() {
+
+	var tabPanel = function(li) {
+		var selector = $(li).find('a').attr('href');
+		return $(selector);
+	}
+
+	$.fn.tabs = function() {
+
+		var $li = this.find('li'),
+			i, $activeLi;
+
+		for (i=0; i < $li.length; i++) {
+			if (i == 0) {
+				$activeLi = $( $li[i] );
+			} else {
+				var $div = tabPanel( $li[i] )
+				$div.hide();
+			}
+		};
+
+		this.find('li').on('click', function(e) {
+			e.preventDefault();
+
+			if (tabPanel($activeLi).selector !== tabPanel(this).selector) {
+				tabPanel($activeLi).hide();
+				tabPanel(this).show();
+				$activeLi = this;
+			}
+		});
+
+	};
+
+})();
+
+// $('#breeds').tabs();
